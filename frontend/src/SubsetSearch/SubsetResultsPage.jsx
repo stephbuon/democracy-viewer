a2436d504acaf73c5561a803e66b49d539b0f620
wesoa012 <wesa@smu.edu>
2023-02-07 11:01:40 -0600

    Updates
    having trouble pressing enter and redirecting user, but now displays a results array (will be gotten from backend later)

--- frontend/src/SubsetSearch/SubsetResultsPage.jsx
+++ frontend/src/SubsetSearch/SubsetResultsPage.jsx
@@ -1,26 +1,32 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { useState, useEffect } from "react";
+import { DisplayResults } from './DisplayResults/DisplayResults';
 
 
-
+const hardcodedResults = ["hello", "helloj", ";lo", "asdkfjh", "k"]
 
 export const SubsetResultsPage = (props) => {
     const navigate = useNavigate();
     const params = useParams()
 
     const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
 
     const searchFunction = () => {
+        console.log("navigating")
         navigate(`/subsetsearch/${searchTerm}`)
+        console.log("did not navigate from results page")
+
     }
 
     //code to see if enter key is pressed (search when that happens)
     useEffect(() => {
         setSearchTerm(params.searchterm)
         const keyDownHandler = event => {
-            console.log('User pressed: ', event.key);
+            // console.log('User pressed: ', event.key);
 
             if (event.key === 'Enter') {
+                console.log('User pressed: Enter');
                 searchFunction();
             }
         };
@@ -38,6 +44,13 @@ export const SubsetResultsPage = (props) => {
             <input type="text" id='searchTerm' value={searchTerm} onChange={event => { setSearchTerm(event.target.value) }} />
             <button type='button' onClick={() => searchFunction()}>Search</button>
         </row>
+
+        <div className='mt-5'></div>
+
+        <DisplayResults
+        results={hardcodedResults}/>
+        
+        
     </div >)
 
 }
