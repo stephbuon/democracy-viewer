5d2256d528963a8a4981a757a305ee9dbfc911c0
wesoa012 <wesa@smu.edu>
2023-02-12 09:06:24 -0600

    adding mui components

--- frontend/src/SubsetSearch/DisplayResults/DisplayResults.jsx
+++ frontend/src/SubsetSearch/DisplayResults/DisplayResults.jsx
@@ -1,6 +1,9 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { useState, useEffect } from "react";
 
+//MUI imports
+import Box from '@mui/material/Box';
+
 
 import './DisplayResults.css';
 export const DisplayResults = (props) => {
@@ -12,11 +15,23 @@ export const DisplayResults = (props) => {
     }, []);
 
 
-    return (<div>
+    return (<Box
+        mt={'5em'}
+        sx={{
+            display: "flex"
+        }}
+    >
         {results.map(() => {
-            return <div className="displayRow">
+            return <Box 
+                className="displayRow"
+                sx={{
+                    
+                    minWidth: '80%'
+                }}
+            >
                 Display results here? Not exactly sure how this should go down
-            </div>})}
-    </div >)
+            </Box>
+        })}
+    </Box >)
 
 }
