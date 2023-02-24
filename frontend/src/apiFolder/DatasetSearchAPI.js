6d9176329fa2673e5821ac87868402805ed884d1
wesoa012 <wesa@smu.edu>
2023-02-23 19:12:44 -0600

    Now hooked up to the backend

--- frontend/src/apiFolder/DatasetSearchAPI.js
+++ frontend/src/apiFolder/DatasetSearchAPI.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 export const BACKEND_ENDPOINT = "http://localhost:8000";
 
 export const FilterDatasets = async (params) =>  {
-    console.log("Filtering Datasets");
+    console.log("Filtering Datasets", params);
 
-    const res = await axios.get(`${BACKEND_ENDPOINT}/datasetsearch`);
+    const res = await axios.get(`${BACKEND_ENDPOINT}/datasets/filter?type=${params.type}&search=${params.searchTerm}`);
     if(res.status !== 200){
         console.log(`Couldn't get datasets information. ${res.status}`, params)
         return null;
     }
+    console.log("Returning", res.data);
     return res.data;
 };
