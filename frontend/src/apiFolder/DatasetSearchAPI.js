2df36deb34ea131e5dd2931982c9feb635d5e378
wesoa012 <wesa@smu.edu>
2023-02-27 16:39:09 -0600

    Allowing for more filter options

--- frontend/src/apiFolder/DatasetSearchAPI.js
+++ frontend/src/apiFolder/DatasetSearchAPI.js
@@ -4,8 +4,7 @@ export const BACKEND_ENDPOINT = "http://localhost:8000";
 
 export const FilterDatasets = async (params) =>  {
     console.log("Filtering Datasets", params);
-
-    const res = await axios.get(`${BACKEND_ENDPOINT}/datasets/filter?type=${params.type}&search=${params.searchTerm}`);
+    const res = await axios.get(`${BACKEND_ENDPOINT}/datasets/filter?type=${params.type}&search=${params.searchTerm}&title=${params.title}&username=${params.username}${params.tags}`);
     if(res.status !== 200){
         console.log(`Couldn't get datasets information. ${res.status}`, params)
         return null;
