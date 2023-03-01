61f599ec5386e28eccff13cda9066aaa2a8bd17a
wesoa012 <wesa@smu.edu>
2023-03-01 17:14:03 -0600

    will parse previously used dataset from local storage
    Not sure if this should be only for logged in user or what

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,11 +11,28 @@ import { DatasetResultsPage } from "./DatasetSearch/DatasetResultsPage";
 export const App = () => {
   
   const [dataset, setDataset] = useState(undefined);
+
+  const chooseDataset = (choice) =>{
+    setDataset(choice)
+    localStorage.setItem('dataset', JSON.stringify(choice))
+  }
+
+  useEffect(() => {
+    console.log("Strating Democracy Viewer App")
+
+    //TODO implement this (only when logged in?)
+    if(localStorage.getItem('dataset') != undefined)
+    {
+      setDataset(JSON.parse(localStorage.getItem('dataset')))
+    }
+  },[]);
   
   useEffect(()=>{
     console.log("NOW USING NEW DATASET", dataset);
   }, [dataset]);
+
   
+
   return (
     <div className={`App`} >
       <BrowserRouter>
@@ -30,7 +47,7 @@ export const App = () => {
 
           <Route
             path='/datasetsearch'
-            element={<DatasetResultsPage setDataset={(x) => setDataset(x)}/>} />
+            element={<DatasetResultsPage setDataset={(x) => chooseDataset(x)}/>} />
         </Routes>
       </BrowserRouter>
     </div>
