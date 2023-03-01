859c7e8c12242f7e9cdc32238aaaf863b39e7b83
wesoa012 <wesa@smu.edu>
2023-03-01 13:24:50 -0600

    Displays tags in a better way
    Still not perfect, but functional for now

--- frontend/src/DatasetSearch/Result.jsx
+++ frontend/src/DatasetSearch/Result.jsx
@@ -2,14 +2,17 @@
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Table from '@mui/material/Table';
+import Button from '@mui/material/Button';
 import { TableBody, TableHead, FormControl, MenuItem, Select, InputLabel, TableRow, TableCell } from '@mui/material';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useState, useEffect } from "react";
 
-export const Result = (result) => {
+export const Result = (props) => {
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    
+    const navigate = useNavigate();
     useEffect(() => {
         // console.log(result)
 
@@ -20,9 +23,9 @@ export const Result = (result) => {
         <Box onClick={() => handleOpen()}
             sx={{
                 background: 'rgb(255, 255, 255)',
-                color: 'rgb(0, 0, 0)'
+                color: 'rgb(0, 0, 0)',
             }}>
-            {result.result.title}
+            {props.result.title}
         </Box>
         <Modal
             open={open}
@@ -41,24 +44,21 @@ export const Result = (result) => {
                     borderRadius: ".5em .5em"
                 }}
             >
-                <Table
-                sx={{
-                    margin: "1em"
-                }}>
+                <Table>
                     <TableHead>
                         <TableRow>
                             <TableCell
                                 sx={{
 
                                 }}>
-                                {result.result.title}
+                                {props.result.title}
                             </TableCell>
                             <TableCell>
                                 &nbsp;
                             </TableCell>
                             <TableCell>
-                                {result.result.is_public && "Public"}
-                                {!result.result.is_public && "Private"}
+                                {props.result.is_public && "Public"}
+                                {!props.result.is_public && "Private"}
                             </TableCell>
                         </TableRow>
                     </TableHead>
@@ -68,33 +68,69 @@ export const Result = (result) => {
                                 Owner:
                             </TableCell>
                             <TableCell>
-                                {result.result.username}
+                                {props.result.username}
                             </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>
-                                Tags:
-                            </TableCell>
-                            {result.result.tags.map((tag, index) => {
-                                {
-                                    if (index < 5) {
-                                        return <TableCell key={index}>
-                                            {tag}
-                                        </TableCell>
-                                    }
-                                }
-                            })}
+                            <TableCell />
                         </TableRow>
                         <TableRow>
                             <TableCell>
                                 Description:
                             </TableCell>
                             <TableCell>
-                                {result.result.description}
+                                {props.result.description}
                             </TableCell>
+                            <TableCell />
                         </TableRow>
                     </TableBody>
                 </Table>
+                <Table>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>
+                                Tags:
+                            </TableCell>
+                            {props.result.tags.map((tag, index) => {
+                                if (index < 5) {
+                                    return <TableCell key={index}>
+                                        {tag}
+                                    </TableCell>
+                                }
+                            })}
+                            {props.result.tags.length < 1 && <TableCell key={1} />}
+                            {props.result.tags.length < 2 && <TableCell key={2} />}
+                            {props.result.tags.length < 3 && <TableCell key={3} />}
+                            {props.result.tags.length < 4 && <TableCell key={4} />}
+                            {props.result.tags.length < 5 && <TableCell key={5} />}
+                            {props.result.tags.length < 6 && <TableCell key={6} />}
+                            {props.result.tags.length > 5 && <TableCell key={'...'}>
+                                ...
+                            </TableCell>}
+
+                        </TableRow>
+                    </TableBody>
+                </Table>
+                <Box 
+                sx={{
+                    width:'100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    marginTop: '2em'
+                }}>
+                <Button
+                    variant="contained"
+                    primary
+                    sx={{
+                        
+                    }}
+                    onClick={() => {
+                        props.setDataset(props.result);
+                        navigate('/subsetSearch');
+                    }}
+                >
+                    Use Dataset
+                </Button>
+                </Box>
+                
             </Box>
         </Modal>
 
