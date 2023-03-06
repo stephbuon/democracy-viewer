233016835701b0cb7142c728fb432910c2799f03
wesoa012 <wesa@smu.edu>
2023-03-06 11:15:51 -0600

    Mostly done

--- frontend/src/SubsetSearch/SubsetResultsPage.jsx
+++ frontend/src/SubsetSearch/SubsetResultsPage.jsx
@@ -1,14 +1,20 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { useState, useEffect } from "react";
-import { DisplayResults } from './DisplayResults/DisplayResults';
 
 //MUI Imports
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Modal from '@mui/material/Modal';
+import Table from '@mui/material/Table';
+import { TableBody, TableHead, FormControl, MenuItem, Select, InputLabel, TableRow, TableCell } from '@mui/material';
+
+//Other Imports
 import { GetSubsetOfData } from '../apiFolder/SubsetSearchAPI';
 
-
+import "./MoveBar.css"
+import "./Loading.css"
+import { Result } from './Result';
 
 export const SubsetResultsPage = (props) => {
     const navigate = useNavigate();
@@ -17,19 +23,50 @@ export const SubsetResultsPage = (props) => {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
     const [searched, setSearched] = useState(false);
+    const [searching, setSearching] = useState(false);
 
-    const searchFunction = () => {
-        console.log("navigating")
-        navigate(`/subsetsearch/${searchTerm}`)
-        console.log("did not navigate from results page")
+    const [loadingResults, setLoadingResults] = useState(false);
+
+    const doMoveAnimation = () => {
+        console.log("STARTING THE MOVE")
+        if(!searched)
+        {
+            setSearching(true)
+            setTimeout(() => finishAnimation(), 500);
+        }
+        else
+        {
+            setLoadingResults(true);
+            setTimeout(() => setLoadingResults(false), 3000);
+        }
 
     }
+    const finishAnimation = () => {
+        setSearched(true)
+        setSearching(false);
+        setLoadingResults(true);
+        fetchSubset();
+    }
+
+    const fetchSubset = () => {
+        setTimeout(() => setLoadingResults(false), 3000);
+        let query = {
+            table_name: props.dataset.table_name,
+            search: ''
+        }
+        GetSubsetOfData(query).then((res) => {
+            setSearchResults(res);
+            setLoadingResults(false);
+        })
+    }
+
     const handleKeyPress = event => {
         // console.log('User pressed: ', event.key);
 
         if (event.key === 'Enter') {
+            doMoveAnimation()
             console.log(searchTerm)
-            navigate(`/subsetsearch/${searchTerm}`);
+            
         }
     };
 
@@ -44,51 +81,128 @@ export const SubsetResultsPage = (props) => {
 
 
     return (<div className='darkblue'>
-        <Box
-            pt={2}
+        <Box className={`${searching ? 'searching-parent' : ''} ${searched ? 'searched' : 'not-searched'}`}
             sx={{
-                background: 0xffffffff,
-                display: "flex",
-                alignItems: 'center',
-                justifyContent: 'center'
+                // transform: 'translate(0, -45vh)',
+                display: 'flex',
+                // height: "100vh",
+                // paddingTop: '2%'
+                justifyContent: "center",
+                alignItems: "center"
             }}
         >
-            <Box
+            <Box className = {`${searching ? 'searching' : ''} ${searched ? 'searched-bar' : 'not-searched-bar'}`}
                 sx={{
-                    borderRadius: '.5em',
-                    overflow: "hidden"
-                }}>
-                <TextField
-                    id="searchTerm"
-                    label="Search"
-                    variant="filled"
+                    display: 'flex',
+                    // display: 'flex',
+                }}
+            >
+                <Box
+                    sx={{
+                        borderRadius: '.5em',
+                        overflow: "hidden",
+                        width: '100%',
+                        
+                    }}>
+                    <TextField
+                        id="searchTerm"
+                        label="Search"
+                        variant="filled"
+                        fullWidth
+                        sx={{
+                            background: 'rgb(255, 255, 255)',
+                            color: 'rgb(0, 0, 0)',
+                    
+
+                        }}
+                        value={searchTerm}
+                        onChange={event => { setSearchTerm(event.target.value) }}
+                        // New Code to search with enter press
+                        onKeyPress={event => handleKeyPress(event)}
+                    />
+                </Box>
+                <Button
+                    variant="contained"
                     sx={{
                         background: 'rgb(255, 255, 255)',
                         color: 'rgb(0, 0, 0)',
-                    }}
-                    value={searchTerm}
-                    onChange={event => { setSearchTerm(event.target.value) }}
-                    // New Code to search with enter press
-                    onKeyPress={event => handleKeyPress(event)}
-                />
-            </Box>
-            <Button
-                variant="contained"
-                sx={{
-                    background: 'rgb(255, 255, 255)',
-                    color: 'rgb(0, 0, 0)',
-                    '&:hover': {
-                        background: 'rgb(200, 200, 200)'
-                    }
-                }}
-                onClick={() => searchFunction()}
-            >
-                Search
-            </Button>
-        </Box>
 
-        <DisplayResults
-            results={searchResults} />
+                        '&:hover': {
+                            background: 'rgb(200, 200, 200)'
+                        }
+                    }}
+                    onClick={() => doMoveAnimation()}
+                >
+                    Search
+                </Button>
+            </Box>
+        </Box>
+        {searched && <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+            }}>
+            <Table
+                sx={{
+                    color: 'rgb(0, 0, 0)',
+                    marginTop: '2rem',
+                    width: .8,
+                    marginBottom: '2rem'
+                }}
+            >
+                <TableHead
+                    sx={{
+                        background: 'rgb(255, 255, 255)',
+                    }}>
+                    <TableRow>
+                        <TableCell>
+                            Results
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+
+                {/*Animated Class while people wait for database response*/}
+                {loadingResults && <TableBody sx={{background: '#fff'}}>
+                    <TableRow className='loadingData1'>
+                        <TableCell>&nbsp;</TableCell>
+                    </TableRow>
+                    <TableRow className='loadingData2'>
+                        <TableCell>&nbsp;</TableCell>
+                    </TableRow>
+                    <TableRow className='loadingData3'>
+                        <TableCell>&nbsp;</TableCell>
+                    </TableRow>
+                    <TableRow className='loadingData4'>
+                        <TableCell>&nbsp;</TableCell>
+                    </TableRow>
+                    <TableRow className='loadingData5'>
+                        <TableCell>&nbsp;</TableCell>
+                    </TableRow>
+                    <TableRow className='loadingData6'>
+                        <TableCell>&nbsp;</TableCell>
+                    </TableRow>
+                    <TableRow className='loadingData7'>
+                        <TableCell>&nbsp;</TableCell>
+                    </TableRow>
+                    <TableRow className='loadingData8'>
+                        <TableCell>&nbsp;</TableCell>
+                    </TableRow>
+                </TableBody>}
+
+                {!loadingResults && <TableBody
+                    sx={{
+                        background: 'rgb(200, 200, 200)'
+                    }}>
+                    {searchResults.map((result) => {
+                        return <TableRow id={result.keys()[0]} key={result.keys()[0]}>
+                            <TableCell>
+                                <Result result={result} dataset={props.dataset}/>
+                            </TableCell>
+                        </TableRow>
+                    })}
+                </TableBody>}
+            </Table>
+        </Box>}
 
     </div >)
 
