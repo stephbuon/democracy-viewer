1e420dfe02b9f75bb774583bfa419be18b75eefe
wesoa012 <wesa@smu.edu>
2023-03-06 11:15:01 -0600

    subset search

--- frontend/src/apiFolder/SubsetSearchAPI.js
+++ frontend/src/apiFolder/SubsetSearchAPI.js
@@ -4,7 +4,7 @@ export const BACKEND_ENDPOINT = "http://localhost:8000";
 
 export const GetSubsetOfData = async (query) =>  {
     console.log("Getting Subset");
-    const res = await axios.get(`${BACKEND_ENDPOINT}/datasets/subset/${query.table_name}&${query.search}`);
+    const res = await axios.get(`${BACKEND_ENDPOINT}/datasets/subset/${query.table_name}${query.search}`);
     if(res.status !== 200){
         console.log(`Couldn't get subset information. ${res.status}`)
         return null;
