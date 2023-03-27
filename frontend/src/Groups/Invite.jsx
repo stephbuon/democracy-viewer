acb2533ad68c9d121dba56fd60cbcee336ef5bcf
wesoa012 <wesa@smu.edu>
2023-03-27 17:57:10 -0500

    able to invite users to a group

--- frontend/src/Groups/Invite.jsx
+++ frontend/src/Groups/Invite.jsx
@@ -1,4 +1,69 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import SendIcon from '@mui/icons-material/Send';
+import { Table, TableBody, TableHead, FormControl, MenuItem, Select, InputLabel, TableRow, TableCell } from '@mui/material';
+
+
+import { useState } from 'react';
+import { SendInvite } from '../apiFolder/GroupAPI';
+
+
 export const Invite = (props) => {
 
-    return <div></div>
+    const [username, setUsername] = useState('');
+    const [sentInvite, setSentInvite] = useState(false)
+
+    const SendIn = (invite) => {
+        SendInvite(invite).then(async (res) => {
+            setSentInvite(true);
+        })
+    }
+
+    return <div>
+        <Box
+            sx={{
+                position: 'absolute',
+                top: '15%',
+                left: '15%',
+                height: "70%",
+                overflow: "scroll",
+                width: "70%",
+                bgcolor: 'background.paper',
+                border: '1px solid #000',
+                borderRadius: ".5em .5em",
+                
+                justifyContent: "center",
+                alignItems: "center"
+            }}
+        >
+            <Table>
+                <TableBody>
+                    <TableRow >
+                        <TableCell >
+                            <TextField
+                                sx={{ marginTop: '2em' }}
+                                label="Username"
+                                variant="filled"
+                                value={username}
+                                onChange={event => { setUsername(event.target.value) }}
+                            />
+                        </TableCell>
+                    </TableRow>
+
+                    <TableRow >
+                        <TableCell >
+                            <Button
+                                // endIcon={SendIcon}
+                                onClick={() => SendIn({ username, private_group: props.groupid })}
+                            >
+                                Send
+                            </Button>
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+            {/* {sentInvite && <LoadingButton></LoadingButton>} */}
+        </Box>
+    </div>
 }
