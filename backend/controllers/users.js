57af9c83bddfd1b82d073febf4bbe9fdd7e2946a
Ryan Schaefer <rschaefer1@me.com>
2023-03-31 11:34:58 -0500

    Fix docker

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 require('dotenv').config();
 
 const accessTokenSecret = process.env.TOKEN_SECRET;
