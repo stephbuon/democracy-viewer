14cbb875ab7ddb53fa30a857350a01877b4e52aa
Ryan Schaefer <rschaefer1@me.com>
2023-11-16 13:20:16 -0600

    Working on parallel preprocessing

--- backend/preprocessing/util/split_text.R
+++ backend/preprocessing/util/split_text.R
@@ -1,24 +1,18 @@
-library(dhmeasures)
-library(dplyr)
-library(textstem)
+library(dhmeasures, quietly = TRUE)
+library(dplyr, quietly = TRUE)
+library(textstem, quietly = TRUE)
+options(dplyr.summarise.inform = FALSE)
 
 # Split all text columns into word counts
-split_text = function(data, text_cols) {
-    all_tokens = list()
-    
-    for (i in 1:length(text_cols)) {
-      curr = data %>%
-        dhmeasures::count_tokens(text = text_cols[i], group = "id") %>%
-        dhmeasures::remove_stop_words() %>%
-        dplyr::mutate(word = textstem::lemmatize_words(word)) %>%
-        dplyr::group_by(id, word) %>%
-        dplyr::summarise(n = sum(n)) %>%
-        dplyr::ungroup() %>%
-        dplyr::mutate(col = text_cols[i])
-      all_tokens[[i]] = curr
-    }
-    
-    final = do.call(rbind, all_tokens)
-    
-    return(final)
+split_text = function(data, text_col) {
+  tokens = data %>%
+    dhmeasures::count_tokens(text = text_col, group = "id") %>%
+    dhmeasures::remove_stop_words() %>%
+    dplyr::mutate(word = textstem::lemmatize_words(word)) %>%
+    dplyr::group_by(id, word) %>%
+    dplyr::summarise(n = sum(n)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(col = text_col)
+  
+  return(tokens)
 }
