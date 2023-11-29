6b09ae7c9288304c9de653bea7c6c0f5c77dabdb
Ryan Schaefer <rschaefer1@me.com>
2023-11-28 21:48:49 -0600

    Fix counts graph generation

--- backend/graphs/util/counts.py
+++ backend/graphs/util/counts.py
@@ -1,13 +1,6 @@
-# These will let us use R packages:
-from rpy2.robjects import pandas2ri
-from rpy2.robjects import conversion
-from rpy2.robjects.packages import STAP
-
-pandas2ri.activate()
-
 def counts(data, word_list, word):
     # Filter for words in word_list
-    output = data[data[word] in word_list]
+    output = data[data[word].isin(word_list)]
     
     return output
 
