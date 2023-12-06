9b3eb394d237c10c9b193b6673e6373bd9683a4c
wesoa012 <wesa@smu.edu>
2023-12-06 04:13:09 -0600

    Dataset filtering by tags
    Done and done. Can filter by tags now

--- frontend/src/apiFolder/DatasetSearchAPI.js
+++ frontend/src/apiFolder/DatasetSearchAPI.js
@@ -2,6 +2,16 @@ import axios from 'axios';
 
 export const BACKEND_ENDPOINT = "http://localhost:8000";
 
+const apiConfig = () => {
+    let demoV = JSON.parse(localStorage.getItem('democracy-viewer'));
+    return {
+        headers:{
+            Authorization: `Bearer ${ demoV.user.token }`
+            //'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJkc2NoYWVmZXIiLCJlbWFpbCI6InJkc2NoYWVmZXJAc211LmVkdSIsInRpdGxlIjoiU3R1ZGVudCIsImZpcnN0X25hbWUiOiJSeWFuIiwibGFzdF9uYW1lIjoiU2NoYWVmZXIiLCJzdWZmaXgiOm51bGwsIm9yY2lkIjoiMDAwMDAwMDE3Njk0Mzk5NCIsImxpbmtlZGluX2xpbmsiOiJodHRwczovL3d3dy5saW5rZWRpbi5jb20vaW4vcnlhbmRzY2hhZWZlci8iLCJpYXQiOjE2NzY0MzMyNzd9.L9Ue0CBU3QkPFUrKdxlKr2loXNWoeK-pDFkC9eFgOng'
+        }
+    }
+  };
+
 export const FilterDatasets = async (params, page) =>  {
     console.log("Filtering Datasets", params);
     // console.log(`GET  -  ${BACKEND_ENDPOINT}/datasets/filter?type=${params.type}${params.searchTerm}`)
@@ -52,7 +62,7 @@ export const GetAllTags = async () =>  {
     console.log("Getting Tags");
     const res = await axios.get(`${BACKEND_ENDPOINT}/datasets/tags/unique`);
     if(res.status !== 200){
-        console.log(`Couldn't get tags. ${res.status}`)
+        console.log(`Couldn't get tags. ${res.status}`, apiConfig())
         return null;
     }
     console.log("Returning", res.data);
