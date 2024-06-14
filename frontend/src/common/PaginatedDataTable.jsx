ca271aa16e8750339d2e088dfc5025e15fd53d61
Ryan Schaefer <rschaefer1@me.com>
2024-06-13 21:39:44 -0500

    Working frontend suggestions

--- frontend/src/common/PaginatedDataTable.jsx
+++ frontend/src/common/PaginatedDataTable.jsx
@@ -1,6 +1,5 @@
 //MUI Imports
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import { Box, Button, TextField, Tooltip, Checkbox, FormControlLabel } from '@mui/material';
 
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
@@ -16,6 +15,9 @@ export const PaginatedDataTable = ({ searchResults, page, totalNumOfPages, GetNe
     const [ editStart, setEditStart ] = useState(-1);
     const [ editEnd, setEditEnd ] = useState(-1);
     const [ editText, setEditText ] = useState("");
+    const [ newText, setNewText ] = useState("");
+    const [ disabled, setDisabled ] = useState(true);
+    const [ suggest, setSuggest ] = useState(false);
 
     const renderPageNumbers = () => {
         const pageNumbers = [];
@@ -53,36 +55,74 @@ export const PaginatedDataTable = ({ searchResults, page, totalNumOfPages, GetNe
         ));
     };
 
+    const getCellClick = (event) => {
+        const cell = event.target;
+        setClickRow(cell.closest('tr').rowIndex - 1);
+        setClickCol(cell.cellIndex);
+    }
+
+    const getSelection = (event) => {
+        const cell = event.target;
+        const allText = cell.innerText;
+        const selection = window.getSelection();
+        const start_ = selection.anchorOffset;
+        const end_ = selection.focusOffset;
+        if (start_ - end_ !== 0) {
+            const start = Math.min(start_, end_);
+            const end = Math.max(start_, end_);
+            const text = allText.substring(start, end);
+            setEditStart(start);
+            setEditEnd(end);
+            setEditText(text);
+            setNewText(text);
+            setEditOpen(true);
+        }
+    }
+
     useEffect(() => {
         const cells = document.querySelectorAll(".p-datatable-wrapper td");
-        cells.forEach(cell => {
-            cell.addEventListener('click', () => {
-                setClickRow(cell.closest('tr').rowIndex - 1);
-                setClickCol(cell.cellIndex);
+        if (suggest) {
+            cells.forEach(cell => {
+                cell.addEventListener('click', getCellClick);
+                cell.addEventListener('mouseup', getSelection);
             });
+        } else {
+            cells.forEach(cell => {
+                cell.removeEventListener('click', getCellClick);
+                cell.removeEventListener('mouseup', getSelection);
+            });
+        }
 
-            cell.addEventListener('mouseup', () =>
-            {
-                console.log(cell)
-                const allText = cell.innerText;
-                const selection = window.getSelection();
-                const start_ = selection.anchorOffset;
-                const end_ = selection.focusOffset;
-                const start = Math.min([ start_, end_ ]);
-                const end = Math.max([ start_, end_ ]);
-                setEditStart(start);
-                setEditEnd(end);
-                setEditText(allText.substring(start, end));
-                setEditOpen(true);
-            })
-        });
-    }, [searchResults]);
+        return () => {
+            cells.forEach(cell => {
+                cell.removeEventListener('click', getCellClick);
+                cell.removeEventListener('mouseup', getSelection);
+            });
+        }
+    }, [searchResults, suggest]);
+
+    useEffect(() => {
+        if (newText === editText) {
+            setDisabled(true);
+        } else {
+            setDisabled(false);
+        }
+    }, [newText])
 
     if (!searchResults || searchResults.length === 0) {
         return <></>
     }
 
     return <>
+        <Tooltip arrow title = "Highlight text to suggest changes to the dataset">
+            <FormControlLabel 
+                control={<Checkbox defaultChecked={suggest}/>} 
+                label="Suggest Changes" 
+                onChange={event => setSuggest(!suggest)} 
+                style = {{ marginLeft: "100px" }}
+            />
+        </Tooltip>
+
         <DataTable value={searchResults} scrollable scrollHeight="750px" showGridlines stripedRows style = {{ marginLeft: "100px" }}>
             {
                 Object.keys(searchResults[0]).map((col, i) => (
@@ -104,8 +144,20 @@ export const PaginatedDataTable = ({ searchResults, page, totalNumOfPages, GetNe
             open={editOpen}
             setOpen={setEditOpen}
             titleText={"Edit Dataset Content"}
-            bodyText={""}
+            bodyText={<>
+                <p>Suggest change to text "{ editText }"</p>
+                <TextField
+                    id="Description"
+                    // label="Description"
+                    variant="filled"
+                    fullWidth
+                    sx={{ background: 'rgb(255, 255, 255)' }}
+                    value={newText}
+                    onChange={event => { setNewText(event.target.value); }}
+                />
+            </>}
             action={() => {}}
+            disabled={disabled}
         />
     </>
 }
