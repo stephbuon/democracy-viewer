70129863df05fb3fa0261c449d4fecaea9cac746
Ryan Schaefer <rschaefer1@me.com>
2024-08-04 18:12:27 -0500

    Option 1

--- backend/python/util/s3.py
+++ backend/python/util/s3.py
@@ -4,6 +4,7 @@ from jwt import decode
 from os import environ
 from os.path import exists
 from pandas import DataFrame, read_parquet
+from s3fs import S3FileSystem
 from time import time
 
 BASE_PATH = "../files/s3/{}".format(environ.get("DB_VERSION"))
@@ -105,19 +106,19 @@ def download(folder: str, name: str, token: str | None = None) -> DataFrame:
     if exists(download_path):
         # Do nothing if file already downloaded
         print("{} already exists".format(name))
+        df = read_parquet(download_path, engine = "pyarrow")
     else:
         # Download file from s3
         if "key_" in distributed.keys() and "secret" in distributed.keys():
-            s3_client = client(
-                "s3",
-                aws_access_key_id = distributed["key_"],
-                aws_secret_access_key = distributed["secret"],
-                region_name = distributed["region"]
+            # Create an S3 file system with credentials
+            s3_client = S3FileSystem(
+                key=distributed["key_"],
+                secret=distributed["secret"],
+                client_kwargs={'region_name': distributed["region"]}
             )
         else:
-            s3_client = client(
-                "s3",
-                region_name = distributed["region"]
+            s3_client = S3FileSystem(
+                client_kwargs={'region_name': distributed["region"]}
             )
         if "dir" in distributed.keys():
             path = "{}/{}/{}.parquet".format(distributed["dir"], folder, name)
@@ -125,17 +126,12 @@ def download(folder: str, name: str, token: str | None = None) -> DataFrame:
             path = "{}/{}.parquet".format(folder, name)
             
         start_time = time()
-        s3_client.download_file(
-            distributed["bucket"],
-            path,
-            download_path,
-            Config = config
-        )
+        df = read_parquet("s3://{}/{}".format(distributed["bucket"], path), engine='pyarrow', storage_options={
+            "key":distributed["key_"],
+            "secret":distributed["secret"],
+            "client_kwargs":{'region_name': distributed["region"]}
+        })
         print("Download time: {} minutes".format((time() - start_time) / 60))
-    
-    start_time = time()
-    df = read_parquet(download_path, engine = "pyarrow")
-    print("Conversion time: {} minutes".format((time() - start_time) / 60))
     
     return df
 
